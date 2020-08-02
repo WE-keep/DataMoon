f79c31bc5a3fdf1610008de4f7467c9c847a3bf6
WE-keep <w.e9999@outlook.com>
2020-08-03 00:59:44 +0800

    day02操作运算符的的提交

---
+++ src/java_basic/day02/Operation.java
@@ -0,0 +1,9 @@
+package java_basic.day02;
+
+public class Operation {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println(a + b);
+    }
+}
