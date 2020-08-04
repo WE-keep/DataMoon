2c49f5cbe8d617f0928042081bad2a94b194a551
WE-keep <W.E9999@outlook.com>
2020-08-04 15:21:42 +0800

    2020/8/4 15:21

---
+++ src/java_basic/day09/Demo4/Demo1.java
@@ -0,0 +1,30 @@
+package java_basic.day09.Demo4;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Person person = new Student();
+        System.out.println(person.name);
+        person.show();
+    }
+}
+abstract class Person {
+    String name = "caoca";
+    public abstract void sleep();
+    public void show() {
+        System.out.println("show ING");
+    }
+}
+
+class Student extends  Person {
+    @Override
+    public void sleep() {
+        System.out.println("早早的睡觉");
+    }
+}
+
+class Teacher extends Person {
+    @Override
+    public void sleep() {
+        System.out.println("备完课再睡觉");
+    }
+}
